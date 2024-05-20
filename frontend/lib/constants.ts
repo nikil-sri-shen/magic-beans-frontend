16611fb2bff6cbaa9f6356b884785f49e1eadbb9
Nikil Sri Shen R <nikilsrishen@192.168.1.202>
2024-05-20 09:14:56 +0530

    first commit

---
+++ frontend/lib/constants.ts
@@ -0,0 +1,8 @@
+import { AuthOptions } from "@stacks/connect";
+
+export const appDetails: AuthOptions["appDetails"] = {
+  name: "Beanstalk Exchange",
+  icon: "https://freesvg.org/img/youk-k-Beanstalk.png",
+}
+
+export const contractOwnerAddress = "ST1XBPQQWGEENAGH9RTBVP3JKM91R4RWZ1XAW01PE"
